c981ba0b410354fc59795d747c8b36cf3d3fa19b
Nathanicha Lwd <35954605+nathanicha@users.noreply.github.com>
2022-02-04 10:20:56 +0700

    feat: add editorconfig

--- app/src/main/java/com/natlwd/klintplayground/MainActivity.kt
+++ app/src/main/java/com/natlwd/klintplayground/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.natlwd.klintplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun test(): String? {
+        return "test"
     }
 }
